fdfb5c466776bf65f1f04a7b1e37a9416873008d
Angel Eduardo Vazquez Garcia <angel.eduardo.v.g@hotmail.com>
2018-08-29 23:29:17 -0500

    Tarea 3 Laravel
    Creación del proyecto de laravel y integración cambio de nombre del proyecto y llave de la aplicación.
    Es probable que la llave no se suba, pero el comando es sencillo "php artisan key:generate" se genera la llave

---
+++ Tarea 3/ejercicio/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tarea_3\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Tarea_3\Events\Event' => [
+            'Tarea_3\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
